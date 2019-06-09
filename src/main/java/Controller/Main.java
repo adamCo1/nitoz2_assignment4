ec9b8d11151a309f487ff1542888644099d2c987
adam <adam388cohen@gmail.com>
2019-06-09 17:49:38 +0300

    created 3 windows for UCS

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,18 +1,30 @@
 package Controller;
 
+import Objects.StageHolder;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainWindowFXML.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getClassLoader().getResource("MainWindowFXML.fxml"));
         primaryStage.setTitle("assignment4");
         primaryStage.setScene(new Scene(root, 500, 600));
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                System.exit(0);
+            }
+        });
+
         primaryStage.show();
     }
 
--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -4,7 +4,15 @@ import Event.*;
 import Event.EventDetailsContainer;
 import Event.JoinRequestContainer;
 import Interfaces.ObserveableObject;
-import javafx.beans.InvalidationListener;
+import Objects.StageHolder;
+import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.util.List;
 import java.util.Observable;
@@ -12,12 +20,42 @@ import java.util.Observer;
 
 public class View extends Observable implements IView , ObserveableObject {
 
-    public View(){
+    private Stage primaryStage ;
 
+    public View(){
+    }
+
+    public void setPrimaryStage(Stage primaryStage){
+        this.primaryStage = primaryStage ;
+        this.primaryStage.show();
     }
 
     @Override
     public EventDetailsContainer getEventUpdateFromUser() {
+
+        FXMLLoader loader = new FXMLLoader();
+        try{
+            Parent root = loader.load(getClass().getClassLoader().getResource("UpdateEventFXML.fxml"));
+            Scene scene = new Scene(root);
+            //TO DO - add css
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Update event");
+            stage.setResizable(false);
+            StageHolder.getInstance().holdStage(stage);
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    StageHolder.getInstance().getStage() ;
+                }
+            });
+
+            stage.showAndWait();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -26,7 +64,31 @@ public class View extends Observable implements IView , ObserveableObject {
      * @return EventDetailsContainer object which holds the details of the event
      */
     @Override
-    public EventDetailsContainer getEventDetailsFromUser(List<String> categories) {
+    public EventDetailsContainer getEventDetailsFromUser(String[] categories) {
+
+        FXMLLoader loader = new FXMLLoader();
+        try{
+            Parent root = loader.load(getClass().getClassLoader().getResource("CreateEventFXML.fxml"));
+            Scene scene = new Scene(root);
+            ((ChoiceBox)root.getChildrenUnmodifiable().get(5)).setItems(FXCollections.observableArrayList(categories));
+            //TO DO - add css
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Add event");
+            stage.setResizable(false);
+            StageHolder.getInstance().holdStage(stage);
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    StageHolder.getInstance().getStage() ;
+                }
+            });
+
+            stage.showAndWait();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         /**
          * show the create new event window . change controller when all loaded successfully
@@ -36,6 +98,30 @@ public class View extends Observable implements IView , ObserveableObject {
 
     @Override
     public JoinRequestContainer getUserJoinRequest() {
+
+        FXMLLoader loader = new FXMLLoader();
+        try{
+            Parent root = loader.load(getClass().getClassLoader().getResource("JoinForcesFXML.fxml"));
+            Scene scene = new Scene(root);
+            //TO DO - add css
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Join Force event");
+            stage.setResizable(false);
+            StageHolder.getInstance().holdStage(stage);
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    StageHolder.getInstance().getStage() ;
+                }
+            });
+
+            stage.showAndWait();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         /**
          * show the join forces window . change controller when all loaded successfully
          */
@@ -45,6 +131,7 @@ public class View extends Observable implements IView , ObserveableObject {
 
     @Override
     public Event getEventToUpdateFromUser() {
+
 
         /**
          * show the update event window . change controller when all loaded successfully
